d76554202f5a859a83a4bf583a986625e49581d2
Mikul17 <84858817+Mikul17@users.noreply.github.com>
2024-04-08 03:00:37 +0200

    optimized code

--- Lab2/cmake-build-debug/cmake_install.cmake
+++ Lab2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/miql/Desktop/Lab2
+# Install script for directory: D:/Studia/ISA/SEMESTR 6/KZI/Lab2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/miql/Desktop/Lab2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Studia/ISA/SEMESTR 6/KZI/Lab2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -15,43 +15,50 @@ struct Zadanie{
     Zadanie() : p(0), w(0), d(0) {}
 };
 
+struct Podzadanie {
+    vector<int> kolejnosc;
+    int Cmax = 0;
+};
+
+int obliczKoszt(const Zadanie& zadanie, int Cmax) {
+    return zadanie.w * max(Cmax - zadanie.d, 0);
+}
+
+
 int obliczOptymalneRozwiazanie(const vector<Zadanie>& zadania, vector<int>& najlepszaKolejnosc) {
     int n = zadania.size();
     int N = 1 << n;
     vector<int> F(N, INT_MAX);
-    map<int, vector<int> > poprzednik;
+    vector<Podzadanie> podzadania(N);
+    podzadania[0].Cmax=0;
     F[0] = 0;
 
-    for (int set = 1; set < N; set++) {
-        int Cmax = 0;
-        for (int i = 0; i < n; i++) {
-            if (set & (1 << i)) {
-                Cmax += zadania[i].p;
-            }
-        }
-
-        for (int k = 0; k < n; k++) {
-            if (set & (1 << k)) {
-                int nowyKoszt = F[set - (1 << k)] + zadania[k].w * max(Cmax - zadania[k].d, 0);
-                if (nowyKoszt < F[set]) {
-                    F[set] = nowyKoszt;
-                    poprzednik[set] = poprzednik[set - (1 << k)];
-                    poprzednik[set].push_back(k);
+    for (int podzadanie = 1; podzadanie < N; podzadanie++) {
+        for (int k = 0; k < n; ++k) {
+            if (podzadanie & (1 << k)) {
+                int poprzednie = podzadanie - (1 << k);
+                int Cmax = podzadania[poprzednie].Cmax + zadania[k].p;
+                int nowyKoszt = F[poprzednie] + obliczKoszt(zadania[k], Cmax);
+                if (nowyKoszt < F[podzadanie]) {
+                    F[podzadanie] = nowyKoszt;
+                    podzadania[podzadanie].kolejnosc = podzadania[poprzednie].kolejnosc;
+                    podzadania[podzadanie].kolejnosc.push_back(k);
+                    podzadania[podzadanie].Cmax = Cmax;
                 }
             }
         }
     }
 
-    najlepszaKolejnosc = poprzednik[N - 1];
+    najlepszaKolejnosc = podzadania[N - 1].kolejnosc;
     return F[N - 1];
 }
 
 void wczytajDane(const string& dane, vector<Zadanie>& zadania){
     //windows
-    //ifstream f("C:\\Users\\miql\\Desktop\\Lab2\\witi.data.txt");
+    ifstream f("D:\\Studia\\ISA\\SEMESTR 6\\KZI\\Lab2\\witi.data.txt");
 
     //terminal
-    ifstream f("witi.data.txt");
+    //ifstream f("witi.data.txt");
 
     string tmp;
     int n=0;
@@ -70,6 +77,7 @@ void wczytajDane(const string& dane, vector<Zadanie>& zadania){
     f.close();
 }
 
+
 int main() {
     vector<Zadanie> zadania;
     vector<int> najlepszaKolejnosc;
@@ -83,7 +91,7 @@ int main() {
         cout << "Minimalna suma kar: " << obliczOptymalneRozwiazanie(zadania,najlepszaKolejnosc);
         auto stop = chrono::high_resolution_clock::now();
         auto duration = chrono::duration_cast<chrono::milliseconds>(stop-start);
-        cout << " | Najlepsza kolejność zadań: ";
+        cout << " | Najlepsza kolejnosc zadan: ";
         for(int indeks : najlepszaKolejnosc) {
             cout << indeks + 1 << " ";
         }
