4cf42059d271be0d01b0993b86f793273cea894c
Mikul17 <84858817+Mikul17@users.noreply.github.com>
2024-04-16 23:52:44 +0200

    added cmax

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -11,7 +11,7 @@ struct Zadanie{
 
 void wczytajDane(const string& dane, vector<Zadanie>& zadania){
     //ifstream f("neh.data.txt");
-    ifstream f("/Users/miql/Desktop/KZI/Lab3/neh.data.txt");
+    ifstream f("D:\\Studia\\ISA\\SEMESTR 6\\KZI\\Lab3\\neh.data.txt");
     string tmp;
     int ilosc_zadan;
     int ilosc_maszyn;
@@ -44,14 +44,31 @@ void wyswietlDane(vector<Zadanie>& zadania){
     }
 }
 
-void obliczCmax(vector<int> kolejnosc){
-    
+int obliczCmax(const vector<Zadanie>& zadania, const vector<int>& kolejnosc) {
+    int liczbaMaszyn = zadania.front().zadania.size();
+    vector<int> czasyZakonczeniaMaszyn(liczbaMaszyn, 0);
+
+    for (int idx : kolejnosc) {
+        vector<int> noweCzasyZakonczeniaMaszyn(liczbaMaszyn, 0);
+        for (int m = 0; m < liczbaMaszyn; m++) {
+            int czasZakonczeniaPoprzedniegoZadania = (m == 0) ? 0 : noweCzasyZakonczeniaMaszyn[m - 1];
+            int czasZakonczeniaNaPoprzedniejMaszynie = czasyZakonczeniaMaszyn[m];
+            noweCzasyZakonczeniaMaszyn[m] = max(czasZakonczeniaPoprzedniegoZadania, czasZakonczeniaNaPoprzedniejMaszynie) + zadania[idx - 1].zadania[m];
+        }
+        czasyZakonczeniaMaszyn = noweCzasyZakonczeniaMaszyn;
+    }
+
+    return czasyZakonczeniaMaszyn.back();
 }
+
 
 int main() {
     vector<Zadanie> zadania;
-    string dane = "data.000:\r";
+    string dane = "data.000:";
+    //wersja MacOS
+    //string dane = "data.000\r:";
     wczytajDane(dane,zadania);
     wyswietlDane(zadania);
+    cout<<obliczCmax(zadania,{1,2,3,4})<<endl;
     return 0;
 }
