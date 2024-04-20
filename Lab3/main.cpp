28965fc6fc9067459bb5959742a097c691acf555
Mikul17 <mikulski2016@gmail.com>
2024-04-20 23:31:48 +0200

    neh

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -6,12 +6,14 @@ using namespace std;
 
 struct Zadanie{
     int index;
+    int waga;
     vector<int>zadania;
 };
 
 void wczytajDane(const string& dane, vector<Zadanie>& zadania){
     //ifstream f("neh.data.txt");
-    ifstream f("D:\\Studia\\ISA\\SEMESTR 6\\KZI\\Lab3\\neh.data.txt");
+    //ifstream f("D:\\Studia\\ISA\\SEMESTR 6\\KZI\\Lab3\\neh.data.txt");
+    ifstream f("/Users/miql/Desktop/KZI/Lab3/neh.data.txt");
     string tmp;
     int ilosc_zadan;
     int ilosc_maszyn;
@@ -25,12 +27,15 @@ void wczytajDane(const string& dane, vector<Zadanie>& zadania){
     f>>ilosc_zadan>>ilosc_maszyn;
     for(int i=0;i<ilosc_zadan;i++){
         Zadanie zad;
-        zad.index=i+1;
+        int w=0;
+        zad.index=i;
         for(int i=0; i<ilosc_maszyn; i++){
             int tmp=0;
             f>>tmp;
+            w+=tmp;
             zad.zadania.push_back(tmp);
         }
+        zad.waga=w;
         zadania.push_back(zad);
     }
 }
@@ -40,11 +45,11 @@ void wyswietlDane(vector<Zadanie>& zadania){
         for(int z : zad.zadania){
             cout<<z<<" ";
         }
-        cout<<endl;
+        cout<<" | w: "<<zad.waga<<" |"<<endl;
     }
 }
 
-int obliczCmax(const vector<Zadanie>& zadania, const vector<int>& kolejnosc) {
+int obliczCmaxKolejnosc(const vector<Zadanie>& zadania, const vector<int>& kolejnosc) {
     int liczbaMaszyn = zadania.front().zadania.size();
     vector<int> czasyZakonczeniaMaszyn(liczbaMaszyn, 0);
 
@@ -53,7 +58,7 @@ int obliczCmax(const vector<Zadanie>& zadania, const vector<int>& kolejnosc) {
         for (int m = 0; m < liczbaMaszyn; m++) {
             int czasZakonczeniaPoprzedniegoZadania = (m == 0) ? 0 : noweCzasyZakonczeniaMaszyn[m - 1];
             int czasZakonczeniaNaPoprzedniejMaszynie = czasyZakonczeniaMaszyn[m];
-            noweCzasyZakonczeniaMaszyn[m] = max(czasZakonczeniaPoprzedniegoZadania, czasZakonczeniaNaPoprzedniejMaszynie) + zadania[idx - 1].zadania[m];
+            noweCzasyZakonczeniaMaszyn[m] = max(czasZakonczeniaPoprzedniegoZadania, czasZakonczeniaNaPoprzedniejMaszynie) + zadania[idx].zadania[m];
         }
         czasyZakonczeniaMaszyn = noweCzasyZakonczeniaMaszyn;
     }
@@ -61,14 +66,66 @@ int obliczCmax(const vector<Zadanie>& zadania, const vector<int>& kolejnosc) {
     return czasyZakonczeniaMaszyn.back();
 }
 
+int obliczCmax(const vector<Zadanie>& zadania){
+    int liczbaMaszyn = zadania.front().zadania.size();
+    vector<int> czasyZakonczeniaMaszyn(liczbaMaszyn, 0);
+    for(Zadanie zad : zadania){
+        vector<int> noweCzasyZakonczeniaMaszyn(liczbaMaszyn, 0);
+        for (int m = 0; m < liczbaMaszyn; m++) {
+            int czasZakonczeniaPoprzedniegoZadania = (m == 0) ? 0 : noweCzasyZakonczeniaMaszyn[m - 1];
+            int czasZakonczeniaNaPoprzedniejMaszynie = czasyZakonczeniaMaszyn[m];
+            noweCzasyZakonczeniaMaszyn[m] = max(czasZakonczeniaPoprzedniegoZadania, czasZakonczeniaNaPoprzedniejMaszynie) + zad.zadania[m];
+        }
+        czasyZakonczeniaMaszyn = noweCzasyZakonczeniaMaszyn;
+    }
+    return  czasyZakonczeniaMaszyn.back();
+}
+
+vector<int> indeksy(const vector<Zadanie>& zadania){
+    vector<int> kol;
+    for(const auto & i : zadania){
+        kol.push_back(i.index);
+    }
+    return kol;
+}
+
+int neh(const vector<Zadanie>& dane){
+    vector<Zadanie> posortowane = dane;
+    std::sort(posortowane.begin(), posortowane.end(), [](const Zadanie& a, const Zadanie& b){
+        return a.waga > b.waga;
+    } );
+    vector<int> najlepszaKolejnosc;
+    vector<Zadanie> aktualna;
+    int bestCmax=INT_MAX;
+    for(Zadanie zad : posortowane){
+        aktualna.push_back(zad);
+        bestCmax = obliczCmax(aktualna);
+       for(int j=aktualna.size()-1; j>0 ; j--){
+           if(j!=1){
+               swap(aktualna[j],aktualna[j-1]);
+           }
+           vector<int> indxy = indeksy(aktualna);
+           int currCmax = obliczCmax(aktualna);
+           if( bestCmax > currCmax){
+               bestCmax = currCmax;
+           }
+       }
+    }
+    cout<<endl;
+    wyswietlDane(aktualna);
+    return bestCmax;
+}
+
 
 int main() {
     vector<Zadanie> zadania;
-    string dane = "data.000:";
+    //string dane = "data.000:";
     //wersja MacOS
-    //string dane = "data.000\r:";
+    string dane = "data.000:\r";
     wczytajDane(dane,zadania);
     wyswietlDane(zadania);
-    cout<<obliczCmax(zadania,{1,2,3,4})<<endl;
+    cout <<"Z kol: " <<obliczCmaxKolejnosc(zadania, {0, 1, 2, 3}) << endl;
+    cout <<"Bez kol: " <<obliczCmax(zadania)<<endl;
+    cout<< "NEH: "<<neh(zadania)<<endl;
     return 0;
 }
